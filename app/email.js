32f4be2f978c8606cf7d27035183868a040f6c94
Valeri Karpov <val@karpov.io>
2018-06-12 17:58:38 -0400

    add paypal ipn handler app

---
+++ app/email.js
@@ -0,0 +1,12 @@
+module.exports = props => `
+  <style>
+    ${props.css}
+  </style>
+  <h2>Thanks for Purchasing the <i>Mastering Async/Await</i> Ebook!</h2>
+
+  <p>
+    Your copy of <i>Mastering Async/Await</i> is attached. If you find
+    any typos or have questions, please post them on
+    <a href="https://github.com/vkarpov15/mastering-async-await-issues">this Ebook's GitHub</a>.
+  </p>
+`;
--- app/index.js
+++ app/index.js
@@ -1,6 +1,22 @@
 'use strict';
 
+const email = require('./email');
 const express = require('express');
+const fs = require('fs');
+const inline = require('inline-css');
+
+const mailgunKey = process.env.MAILGUN.trim();
+const mailgunDomain = process.env.MAILGUN_DOMAIN.trim();
+
+const mailgun = require('mailgun-js')({
+  apiKey: mailgunKey,
+  domain: mailgunDomain
+});
+
+const css = fs.readFileSync('./website/style.css').toString();
+const html = inline(email({ css }), { url: 'noop' });
+
+const text = 'Thanks for purchasing "Mastering Async/Await"! Your copy is attached';
 
 run().catch(error => console.error(error.stack));
 
@@ -23,18 +39,28 @@ async function run() {
       return 'IGNORED';
     }
 
+    await mailgun.messages().send({
+      from: 'noreply@asyncawait.net',
+      to: 'val@karpov.io',
+      subject: 'Your Copy of Mastering Async/Await',
+      text,
+      html: await html,
+      attachment: './bin/mastering-async-await.pdf'
+    });
+
     return 'SENT';
   }));
 
-  await app.listen(process.env.PORT || 4000);
-  console.log('Listening');
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
+  console.log(`Listening on ${port}`);
 }
 
 function wrap(fn) {
   return function(req, res) {
     fn(req).then(
       v => res.status(200).send(v),
-      e => res.status(500).send(e.message)
+      e => res.status(500).send(require('util').inspect(e))
     );
   }
 }
