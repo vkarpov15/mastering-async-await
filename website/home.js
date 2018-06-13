fde9dd9d40623b9a6395889d92e1ebdd1ac74175
Valeri Karpov <val@karpov.io>
2018-06-13 16:55:37 -0400

    chore: track clickthroughs to keen

--- website/home.js
+++ website/home.js
@@ -63,7 +63,7 @@ module.exports = props => `
   <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
   <input type="hidden" name="cmd" value="_s-xclick">
   <input type="hidden" name="hosted_button_id" value="PCKRMQJK8KTNQ">
-  <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+  <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" onclick="client.addEvent('track', { type: 'clickthrough' });">
   <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
   </form>
 
