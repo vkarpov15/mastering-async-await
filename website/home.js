c28b313096ee479a1f8cd9d441fa45be27b873d7
Valeri Karpov <val@karpov.io>
2018-06-13 17:19:45 -0400

    more cleanup

--- website/home.js
+++ website/home.js
@@ -60,18 +60,20 @@ module.exports = props => `
 
   <h3>Buy</h3>
 
-  <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
-  <input type="hidden" name="cmd" value="_s-xclick">
-  <input type="hidden" name="hosted_button_id" value="PCKRMQJK8KTNQ">
-  <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" onclick="client.addEvent('track', { type: 'clickthrough' });">
-  <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
-  </form>
+  <div class="buy-button">
+    <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
+    <input type="hidden" name="cmd" value="_s-xclick">
+    <input type="hidden" name="hosted_button_id" value="PCKRMQJK8KTNQ">
+    <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" onclick="client.addEvent('track', { type: 'clickthrough' });">
+    <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+    </form>
+  </div>
 
   <h3>Preview</h3>
 
   <ul>
     <li><a href="/bin/toc.pdf">Table of Contents</a></li>
-    <li><a href="/bin/page-32-33.pdf">Pages 32-33</a></li>
+    <li><a href="/bin/page-30-31.pdf">Pages 30-31</a></li>
   </ul>
 </div>
 `;
