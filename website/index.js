b8c6d409c3861594a6a8c3c6352e1bd4f67fe45f
Valeri Karpov <val@karpov.io>
2018-07-05 16:34:33 -0400

    chore: add wyncode link

--- website/index.js
+++ website/index.js
@@ -3,7 +3,8 @@ const layout = require('./layout');
 
 const pages = [
   { url: '/', path: './index.html', template: './home' },
-  { url: '/thank-you.html', path: './thank-you.html', template: './thanks.js' }
+  { url: '/thank-you.html', path: './thank-you.html', template: './thanks.js' },
+  { url: '/wyncode.html', path: './wyncode.html', template: './wyncode.js' }
 ].map(p => Object.assign(p, { content: require(p.template)(p) }));
 
 for (const page of pages) {
