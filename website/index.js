f9d7d145d59f04db2a71fc27e6a742c70e5812d6
Valeri Karpov <val@karpov.io>
2018-07-10 18:20:03 -0400

    chore: add affiliate page for Alex

--- website/index.js
+++ website/index.js
@@ -4,7 +4,8 @@ const layout = require('./layout');
 const pages = [
   { url: '/', path: './index.html', template: './home' },
   { url: '/thank-you.html', path: './thank-you.html', template: './thanks.js' },
-  { url: '/wyncode.html', path: './wyncode.html', template: './wyncode.js' }
+  { url: '/wyncode.html', path: './wyncode.html', template: './wyncode.js' },
+  { url: '/alex.html', path: './alex.html', template: './alex.js' }
 ].map(p => Object.assign(p, { content: require(p.template)(p) }));
 
 for (const page of pages) {
