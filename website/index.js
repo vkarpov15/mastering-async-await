3e3f67becc6e26b7e9a301b429d16a3d4d6687b5
Valeri Karpov <val@karpov.io>
2018-07-17 15:07:02 -0400

    add jsjabber link

--- website/index.js
+++ website/index.js
@@ -5,7 +5,8 @@ const pages = [
   { url: '/', path: './index.html', template: './home' },
   { url: '/thank-you.html', path: './thank-you.html', template: './thanks.js' },
   { url: '/wyncode.html', path: './wyncode.html', template: './wyncode.js' },
-  { url: '/alex.html', path: './alex.html', template: './alex.js' }
+  { url: '/alex.html', path: './alex.html', template: './alex.js' },
+  { url: '/jsjabber.html', path: './jsjabber.html', template: './jsjabber.js' }
 ].map(p => Object.assign(p, { content: require(p.template)(p) }));
 
 for (const page of pages) {
