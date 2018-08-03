b5c4c4839f8401232845262b2cd73e1d27a9347a
Valeri Karpov <val@karpov.io>
2018-08-03 18:57:30 -0400

    copy improvements

--- website/home.js
+++ website/home.js
@@ -15,10 +15,7 @@ module.exports = ({ price = '$24.99', buyButton = defaultBuyButton }) => `
   <h1 class="title">Mastering Async/Await</h1>
 
   <h3 class="tagline">
-    Learn how
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function">async/await</a>
-    works
-    and how it fits in the JavaScript ecosystem.
+    Become your team's expert on escaping callback hell.
   </h3>
 
   <p>
@@ -48,12 +45,11 @@ module.exports = ({ price = '$24.99', buyButton = defaultBuyButton }) => `
   <p id="author">
     <img src="https://pbs.twimg.com/profile_images/891062607053860864/rDX7vHYT_400x400.jpg" style="float: right; width: 145px; margin-left: 10px">
 
-    Valeri Karpov is the lead maintainer of <a href="https://www.npmjs.com/package/mongoose">Mongoose</a>
-    and the Lead Backend Engineer for
-    <a href="https://www.trybooster.com/">Booster Fuels</a>.
+    Valeri Karpov is the lead maintainer of <a href="https://www.npmjs.com/package/mongoose">Mongoose</a>.
     He's a core contributor to
     <a href="https://www.npmjs.com/package/mocha">Mocha</a>, <a href="https://www.npmjs.com/package/agenda">Agenda</a>,
     and other npm modules with over 10M combined monthly downloads.
+    He was an early engineer at MongoDB (IPO in October 2017) and LevelUp (acquired for $390M in 2018).
     He's the author of <a href="http://es2015generators.com/"><i>The 80/20 Guide to ES2015 Generators</i></a>
     and <a href="https://www.amazon.com/Professional-AngularJS-Valeri-Karpov/dp/1118832078/ref=sr_1_1?ie=UTF8&qid=1528769632&sr=8-1&keywords=professional+angularjs"><i>Professional AngularJS</i></a>.
     <a href="http://thecodebarbarian.com/">His blog</a>
@@ -98,19 +94,19 @@ module.exports = ({ price = '$24.99', buyButton = defaultBuyButton }) => `
     <li><a href="/bin/page-30-31.pdf">Pages 30-31</a></li>
   </ul>
 
-  <h3>Endorsements</h3>
+  <h3>What Developers are Saying</h3>
 
   <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">So I started reading &quot;Mastering Async/Await&quot; by <a href="https://twitter.com/code_barbarian?ref_src=twsrc%5Etfw">@code_barbarian</a> - and just like his &quot;Generators guide&quot; it is an awesome practical tutorial for all the ways you should be using &quot;async/await&quot; in JS. No fluff, straight examples, great text. <a href="https://t.co/nnAEFhtTjj">https://t.co/nnAEFhtTjj</a></p>&mdash; Gleb Bahmutov (@bahmutov) <a href="https://twitter.com/bahmutov/status/1013882918941446146?ref_src=twsrc%5Etfw">July 2, 2018</a></blockquote>
 <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
-  -- <a href="https://glebbahmutov.com/">Gleb Bahmutov</a>, VP of Engineering at <a href="https://www.cypress.io/">Cypress</a>
-
-  <br><br>
+<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">Just bought the &quot;Mastering async/await&quot; book from <a href="https://twitter.com/code_barbarian?ref_src=twsrc%5Etfw">@code_barbarian</a>.<br>Thank you Valeri for putting this together!<br><br>Find it here: <a href="https://t.co/WnsH4tqDld">https://t.co/WnsH4tqDld</a> <a href="https://t.co/KYh5QZROap">pic.twitter.com/KYh5QZROap</a></p>&mdash; Marcus Poehls ⚡️ (@marcuspoehls) <a href="https://twitter.com/marcuspoehls/status/1014520503485259779?ref_src=twsrc%5Etfw">July 4, 2018</a></blockquote>
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
   <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">I&#39;m super impressed with <a href="https://twitter.com/code_barbarian?ref_src=twsrc%5Etfw">@code_barbarian</a> &#39;s latest ebook: Mastering Async/Await. Check it out: <a href="https://t.co/8tsaVfSjsN">https://t.co/8tsaVfSjsN</a></p>&mdash; Brad Vogel (@BradVogel) <a href="https://twitter.com/BradVogel/status/1016473731332390913?ref_src=twsrc%5Etfw">July 10, 2018</a></blockquote>
 <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
-  -- <a href="https://twitter.com/BradVogel">Brad Vogel</a>, CTO of <a href="https://mixmax.com/">Mixmax</a>
+  <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">From the maintainer of my favorite <a href="https://twitter.com/MongoDB?ref_src=twsrc%5Etfw">@MongoDB</a> library <a href="https://twitter.com/mongoosejs?ref_src=twsrc%5Etfw">@mongoosejs</a>, comes a new book giving a deep dive on learning and understanding Async/Await, one of the best new features of es6.<br><br>ch-ch-ch-check it outtt <a href="https://t.co/givAinTbPV">https://t.co/givAinTbPV</a></p>&mdash; Harry Wolff (@hswolff) <a href="https://twitter.com/hswolff/status/1007340590210699265?ref_src=twsrc%5Etfw">June 14, 2018</a></blockquote>
+  <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 </div>
 <div style="clear: both"></div>
 `;
