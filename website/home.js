861bd1294654f66c63cf4bd6b34011f7edbcc852
Valeri Karpov <val@karpov.io>
2021-01-04 17:27:41 -0500

    chore: update price for 2021

--- website/home.js
+++ website/home.js
@@ -1,13 +1,13 @@
 const defaultBuyButton = `
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="PBVTAMSMYXTZN">
+<input type="hidden" name="hosted_button_id" value="DHFGPAJQ25N4C">
 <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
 `;
 
-const defaultPrice = '$27.95';
+const defaultPrice = '$29.99';
 
 module.exports = ({ price = defaultPrice, buyButton = defaultBuyButton }) => `
 <div class="left">
